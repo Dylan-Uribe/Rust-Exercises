f251345c7f7aa826ce65d4c169e8ad367df5fd59
DYLAN JOEL URIBE BLATNIK <a2022112008@estudiantes.upsa.edu.bo>
2024-11-04 17:33:18 -0400

    add project

---
+++ practice_rust/target/debug/deps/practice_rust.d
@@ -0,0 +1,5 @@
+C:\Users\dylan_tmut0bm\RustroverProjects\practice_rust\target\debug\deps\practice_rust.exe: src/main.rs
+
+C:\Users\dylan_tmut0bm\RustroverProjects\practice_rust\target\debug\deps\practice_rust.d: src/main.rs
+
+src/main.rs:
---
+++ practice_rust/target/debug/practice_rust.d
@@ -0,0 +1,1 @@
+C:\Users\dylan_tmut0bm\RustroverProjects\practice_rust\target\debug\practice_rust.exe: C:\Users\dylan_tmut0bm\RustroverProjects\practice_rust\src\main.rs
